1a4fb2738a7a9b57d24bd6f26dc58c6b210a1d7c
Mkenini Zaatout <mkeninismael2@gmail.com>
2017-02-28 16:35:06 +0100

    Ajout projet image

---
+++ Restaurant/src/main/java/fr/unice/iut/restaurant/provider/Entree.java
@@ -0,0 +1,10 @@
+package fr.unice.iut.restaurant.provider;
+
+
+/**
+ * Created by Ismael 28/02/2017.
+ */
+
+public class Entree {
+
+}
---
+++ Restaurant/src/main/java/fr/unice/iut/restaurant/services/MenuImpl.java
@@ -0,0 +1,10 @@
+package fr.unice.iut.restaurant.services;
+
+
+/**
+ * Created by Ismael 28/02/2017.
+ */
+
+public class MenuImpl {
+
+}
