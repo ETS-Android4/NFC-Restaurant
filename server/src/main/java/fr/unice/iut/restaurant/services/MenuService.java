8720c1c6b61f6c82671a2c2d1d286212ee4f53c8
Mkenini Zaatout <mkeninismael2@gmail.com>
2017-03-03 22:36:45 +0100

    edit de Class MenuImpl et MenueService

--- server/src/main/java/fr/unice/iut/restaurant/services/MenuService.java
+++ server/src/main/java/fr/unice/iut/restaurant/services/MenuService.java
@@ -1,10 +1,23 @@
 package fr.unice.iut.restaurant.services;
 
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import fr.unice.iut.restaurant.provider.*;
+import java.sql.SQLException;
 
 /**
  * Created by Ismael 28/02/2017.
  */
 
-public class MenuService {
-
+public interface MenuService {
+	@POST
+	@Path("/plats")
+	@Produces(MediaType.APPLICATION_JSON)
+	Response sendMenu() throws SQLException;
+	/*
+	@POST
+	@Path("/")
+	Plats.GetAllPlats();
+	*/
 }
